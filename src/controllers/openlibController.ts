10bcbc53c3097d4be63a262709415de5cdb44893
Don Shanks <donshanks@gmail.com>
2025-08-18 19:51:08 -0700

    added search book list with adjustable limit

--- src/controllers/openlibController.ts
+++ src/controllers/openlibController.ts
@@ -1,20 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
-import { searchBooks } from '../middlewares/openlib';
+import { getBookList, getBookByISBN, OpenLibBook } from '../middlewares/openlib';
 
 // Search by Book Title
-export const searchByTitle = (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const title = req.params.q;
-        console.log(req.body);
-        const books = [];
-        // books = searchBooks(title);
-        searchBooks(title);
-        // res.json(books);
-    } catch (error) {
-        next(error);
+export const searchByTitle = async (req: Request, res: Response, next: NextFunction) => {
+    const olBookListData = await getBookList(`${req.query.q}`, `${req.query.limit}`);
+    let books: OpenLibBook[] = [];
+
+    for (let i = 0; i < olBookListData.length; i++) {
+        books.push(new OpenLibBook(olBookListData[i]));
     }
+
+    let searchResponse = {
+        timestamp: new Date().toISOString(),
+        function: 'searchByTitle',
+        params: req.query,
+        docs: books
+    }
+    res.json(searchResponse);
 };
 
+
 // Search by Book ISBN
-export const searchByIsbn = (req: Request, res: Response, next: NextFunction) => {
+export const searchByIsbn = async (req: Request, res: Response, next: NextFunction) => {
+    const olBookData: OpenLibBook = await getBookByISBN(`${req.query.q}`);
+
+    let searchResponse = {
+        timestamp: new Date().toISOString(),
+        function: 'searchByIsbn',
+        params: req.query,
+        data: olBookData,
+    }
+    res.json(searchResponse);
 };
--- src/middlewares/openlib.ts
+++ src/middlewares/openlib.ts
@@ -1,36 +1,137 @@
 import axios, { AxiosResponse, AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
-import { title } from 'process';
+import Book from '../models/book';
+import { request } from 'http';
 
-interface Hash {
-    [key: string]: string;
-}
-
-const openlibUrls: Hash = {
+const openlibUrls: UrlHash = {
     "base": "https://openlibrary.org/",
     "covers": "https://covers.copenlibrary.org/",
 };
 
-function getOpenlibClint(baseUrl: string) {
+const getOpenlibClient = (baseUrl: string) => {
     return axios.create({
         baseURL: openlibUrls[baseUrl],
     });
-}
+};
 
-export async function searchBooks(title: string) {
+export const getBookList = async (title: string, limit = "10") => {
     try {
-        const booksClient = getOpenlibClint('base');
-        const jsonResponse: AxiosResponse = await booksClient.get('/search.json', {
+        let bookList = [];
+        const response = await getOpenlibClient('base').get('/search.json', {
             params: {
                 q: title,
-                fields: 'key,title,author_name,editions',
-                limit: 1,
+                limit: limit,
             }
         });
-        console.log(jsonResponse.data);
+        // console.log(response.request.path)
+        return response.data.docs;
     }
     catch (error) {
         console.log(error);
+        return [];
     }
-    return '';
+};
+
+export const getBookByISBN = async (isbn: string) => {
+    try {
+        const response = await getOpenlibClient('base').get(`/isbn/${isbn}.json`);
+        return response.data;
+    }
+    catch (error) {
+        console.log(error);
+        return {};
+    };
+};
+
+export class OpenLibBook {
+    public olid: string;
+    public publishers: string[];
+    public source_records: string[];
+    public title: string;
+    public number_of_pages: number;
+    public covers: number[];
+    public coverKey: string;
+    public isbn_13: string[];
+    public full_title: string;
+    public isbn_10: string[];
+    public publish_date: string;
+    public key: string;
+    public authors: KeyObject[];
+    public authorKeys: string[];
+    public works: KeyObject[];
+    public type: KeyObject;
+    public latest_revision: number;
+    public revision: number;
+    public created: DateTimeObject;
+    public last_modified: DateTimeObject;
+
+    constructor(data: {
+        publishers: string[];
+        source_records: string[];
+        title: string;
+        number_of_pages: number;
+        covers: number[];
+        isbn_13: string[];
+        full_title: string;
+        isbn_10: string[];
+        publish_date: string;
+        key: string;
+        authors: KeyObject[];
+        works: KeyObject[];
+        type: KeyObject;
+        latest_revision: number;
+        revision: number;
+        created: DateTimeObject;
+        last_modified: DateTimeObject;
+    }) {
+        this.publishers = data.publishers;
+        this.source_records = data.source_records;
+        this.title = data.title;
+        this.number_of_pages = data.number_of_pages;
+        this.covers = data.covers;
+        this.isbn_13 = data.isbn_13;
+        this.full_title = data.full_title;
+        this.isbn_10 = data.isbn_10;
+        this.publish_date = data.publish_date;
+        this.key = data.key;
+        this.authors = data.authors;
+        this.works = data.works;
+        this.type = data.type;
+        this.latest_revision = data.latest_revision;
+        this.revision = data.revision;
+        this.created = data.created;
+        this.last_modified = data.last_modified;
+
+        this.olid = '';
+        if (this.key) {
+            this.olid = `${this.key.split(/\//).pop()}`;
+        }
+
+        this.authorKeys = [];
+        if (this.authors) {
+            for (let i = 0; i < this.authors.length; i++) {
+                this.authorKeys.push(this.authors[i].key);
+            }
+        }
+
+        if (this.covers) {
+            this.coverKey = `/b/id/${this.covers[0]}`;
+        }
+        else if (this.olid) {
+            this.coverKey = `/b/olid/${this.olid}`;
+        }
+        else {
+            this.coverKey = '';
+        }
+    }
 }
 
+interface UrlHash {
+    [key: string]: string;
+}
+interface KeyObject {
+    key: string;
+}
+interface DateTimeObject {
+    type: "/type/datetime";
+    value: string;
+}
